4e679ed4427a230a12a2c652a259a27b5099b660
mariaedutt <maria.edutt@hotmail.com>
2024-09-18 17:08:07 -0300

    Finalizando implementação

--- src/index.js
+++ src/index.js
@@ -5,5 +5,10 @@ const app = express();
 
 app.use(express.json());
 app.use(routers);
+app.use((error, request, response, next) => {
+  console.log('#### Error Handler');
+  console.log(error);
+  response.sendStatus(500);
+});
 
 app.listen(3000, () => console.log('🚀 Server started at http://localhost:3000'));
